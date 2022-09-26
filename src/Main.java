1b47300e13b5c58494a5946ebc355ee0a43068f9
gl_sokolov <gl_sokolov@mail.ru>
2022-09-26 23:18:19 +0300

    Выполнено до задания 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
+        // Задание 1
+        byte symbol3 = 123;
+        short symbol5 = 12345;
+        int symbolMnogo = 123456789;
+        long symbolOchenMnogo = 1234567891;
+        float symbolDrob2 = 3.5f;
+        double symbolDrobMnogo = 3.14567;
+        System.out.println("Значение переменной byte с типом");
+
+        // Задание 2
+        double symbol2712 = 27.12;
+        long symbol978678965 = 978678965549L;
+        byte symbol2 = 2;
+        short symbol786 = 786;
+        short symbol569 = 569;
+        short symbol159 = -159;
+        short symbol27897 = 27897;
+        byte symbol67 = 67;
+
+        // Задание 3
+        byte lydkaChildren = 23;
+        byte annaChildren = 27;
+        byte katyaChildren = 30;
+        short paper = 480;
+        int allChildren = lydkaChildren + annaChildren + katyaChildren;
+        int childrenPerOneClass = allChildren / 3;
+        int paperPerChildren = paper / allChildren;
+        System.out.println(paperPerChildren);
+        // Задание 4
+
+        // Задание 5
+
+        // Задание 6
+
+        // Задание 7
+
+        // Задание 8
 
     }
 }
